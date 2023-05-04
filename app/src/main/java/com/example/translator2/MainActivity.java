04a73023086e4e8bc664bd8114a3064c71830845
nPocTocax <1spasivov@mail.ru>
2023-05-04 22:12:24 +0400

    работает

--- app/src/main/java/com/example/translator2/MainActivity.java
+++ app/src/main/java/com/example/translator2/MainActivity.java
@@ -9,12 +9,25 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.InputStream;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
     public Button button;
     public EditText editText;
     public TextView text;
+
+    char[] russian = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к',
+            'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц',
+            'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', ' '};
+    char[] russian2 = {'a', 'b', 'v', 'g', 'd', 'e', 'j', 'z', 'i', 'y', 'k',
+            'l', 'm', 'n', 'o', 'p', 'r', 'c', 't', 'y', 'f', 'h', 'c',
+            '4', 'W', 'W', '6', '7', '8', '9', '3', 'j', ' '};
+
+    String[] morse = {".-", "-...", ".--", "--.", "-..", ".", "...-", "--..", "..", ".---",
+            "-.-", ".-..", "--", "-.", "---", ".--.", ".-.", "...", "-",
+            "..-", "..-.", "....", "-.-.", "---.", "----", "--.-", "--.--", "-.--",
+            "-..-", "..-..", "..--", ".-.-", " "};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,32 +42,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                char[] Russian = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к',
-                        'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц',
-                        'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
-                char[] Russian2 = {'a', 'b', 'v', 'g', 'd', 'e', 'j', 'z', 'i', 'y', 'k',
-                        'l', 'm', 'n', 'o', 'p', 'r', 'c', 't', 'y', 'f', 'h', 'c',
-                        '4', 'W', 'W', '6', '7', '8', '9', '3', 'j'};
 
-                String[] morse = {".-", "-...", ".--", "--.", "-..", ".", "...-", "--..", "..", ".---",
-                        "-.-", ".-..", "--", "-.", "---", ".--.", ".-.", "...", "-",
-                        "..-", "..-.", "....", "-.-.", "---.", "----", "--.-", "--.--", "-.--",
-                        "-..-", "..-..", "..--", ".-.-"};
 
-                String userInput = editText.getText().toString();
+                String userInput = editText.getText().toString().toLowerCase(Locale.ROOT);
+                String str = "";
+                for (int i = 0; i < userInput.length(); i++) {
+                    str += translate(userInput.charAt(i))+" ";
+                            
+
+                }
                 
                 char[] chars = userInput.toCharArray();
 
 
-                /*StringBuilder str = new StringBuilder();
-                for (char aChar : chars) {
-                    for (int j = 0; j < Russian.length; j++) {
 
-                        if (Russian[j] == aChar) {
-                            str.append(morse[j]).append(" ");
-                        }
-                    }
-                }*/
 
                 text.setText(str);
 
@@ -64,6 +65,16 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+    
+    String translate(char letter){
+        for (int i = 0; i < russian2.length; i++) {
+            if (letter == russian2[i]) {
+                return morse[i];
+            }
+        }
+
+        return "@";
     }
 
 
