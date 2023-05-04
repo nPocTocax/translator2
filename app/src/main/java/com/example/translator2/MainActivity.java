ad60fa9e0579d1297ee631a5748f4daf74d92718
nPocTocax <1spasivov@mail.ru>
2023-05-04 21:41:48 +0400

    https://github.com/nPocTocax/translator2.git

--- app/src/main/java/com/example/translator2/MainActivity.java
+++ app/src/main/java/com/example/translator2/MainActivity.java
@@ -8,12 +8,13 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class MainActivity extends AppCompatActivity {
     public Button button;
     public EditText editText;
     public TextView text;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +25,6 @@ public class MainActivity extends AppCompatActivity {
         editText = findViewById(R.id.editText);
         text = findViewById(R.id.text);
 
-
-
-
-
-
-
-
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,22 +32,21 @@ public class MainActivity extends AppCompatActivity {
                 char[] Russian = {'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к',
                         'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц',
                         'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'};
+                char[] Russian2 = {'a', 'b', 'v', 'g', 'd', 'e', 'j', 'z', 'i', 'y', 'k',
+                        'l', 'm', 'n', 'o', 'p', 'r', 'c', 't', 'y', 'f', 'h', 'c',
+                        '4', 'W', 'W', '6', '7', '8', '9', '3', 'j'};
 
                 String[] morse = {".-", "-...", ".--", "--.", "-..", ".", "...-", "--..", "..", ".---",
                         "-.-", ".-..", "--", "-.", "---", ".--.", ".-.", "...", "-",
                         "..-", "..-.", "....", "-.-.", "---.", "----", "--.-", "--.--", "-.--",
                         "-..-", "..-..", "..--", ".-.-"};
 
-
-
-                String userInput = editText.toString();
-
-
-
+                String userInput = editText.getText().toString();
+                
                 char[] chars = userInput.toCharArray();
 
 
-                StringBuilder str = new StringBuilder();
+                /*StringBuilder str = new StringBuilder();
                 for (char aChar : chars) {
                     for (int j = 0; j < Russian.length; j++) {
 
@@ -61,11 +54,7 @@ public class MainActivity extends AppCompatActivity {
                             str.append(morse[j]).append(" ");
                         }
                     }
-                }
-
-
-
-
+                }*/
 
                 text.setText(str);
 
