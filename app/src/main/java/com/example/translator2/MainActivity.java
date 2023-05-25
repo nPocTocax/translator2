15716d77427d2952814d339f875e513304ead613
nPocTocax <1spesivov@mail.ru>
2023-05-25 20:24:51 +0400

    алфавит

--- app/src/main/java/com/example/translator2/MainActivity.java
+++ app/src/main/java/com/example/translator2/MainActivity.java
@@ -7,9 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
